92d4eb29830e17ed768a2c0fcce0953fbf315351
Hui Lee <lihui870920@gmail.com>
2017-11-19 20:03:03 +0800

    Producer&Consumer尝试测试，目前不通

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use Kafka\ConsumerConfig;
+use Kafka\Producer;
+use Kafka\ProducerConfig;
+
+Route::get('/producer', function () {
+    $config = ProducerConfig::getInstance();
+    $config->setMetadataRefreshIntervalMs(10000);
+    $config->setMetadataBrokerList('127.0.0.1:9292');
+    $config->setBrokerVersion('0.10.0.1');
+    $config->setRequiredAck(1);
+    $config->setIsAsyn(false);
+    $config->setProduceInterval(500);
+    $producer = new Producer(function () {
+        return array(
+            array(
+                'topic' => 'test',
+                'value' => 'test kafka message.',
+                'key'   => 'crazy_lee',
+            ),
+        );
+    });
+    $producer->success(function ($result) {
+        dd($result);
+    });
+    $producer->error(function ($errorCode) {
+        dd($errorCode);
+    });
+    $producer->send(true);
+
+    //return view('welcome');
+});
+
+Route::get('/consumer', function () {
+    $config = ConsumerConfig::getInstance();
+    $config->setMetadataRefreshIntervalMs(10000);
+    $config->setMetadataBrokerList('127.0.0.1:9092');
+    $config->setGroupId('test');
+    $config->setBrokerVersion('0.10.2.1');
+    $config->setTopics(array('test'));
+    $config->setOffsetReset('earliest');
+    $consumer = new \Kafka\Consumer();
+    $consumer->start(function ($topic, $part, $message) {
+        var_dump($message);
+    });
 });
